d80968648e4eb3eaaf5467b89d9b15b2b7245764
theLawrenzo1 <thelawrenzo1@gmail.com>
2025-02-06 04:13:02 +0000

    Minor edits to TIP_CALC.PY

--- Day_02/tip_calc.py
+++ Day_02/tip_calc.py
@@ -6,4 +6,4 @@ tip = int(input('How much tip would you like to give 10, 12, or 15? '))
 people = int(input('How many people to split the bill? '))
 tip_in_percent = tip / 100 + 1
 bill_per_person = round(bill * tip_in_percent / people, 2)
-print('Each person should pay ${}'.format(bill_per_person))
+print('Each person should pay: ${}.'.format(bill_per_person))
