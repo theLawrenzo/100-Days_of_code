64c29657d3b607760f7d4f70f9783ded6ea6860d
theLawrenzo1 <thelawrenzo1@gmail.com>
2025-02-08 23:09:24 +0000

    PYTHON FILE - [rock_paper_scissors.py : code modification to test for invalid user entry]

--- Day_04/rock_paper_scissors.py
+++ Day_04/rock_paper_scissors.py
@@ -36,27 +36,27 @@ computer_choice = random.choice(choice_list)
 user_choice = int(input('What do you choose? Type 0 for Rock,'
 			' 1 for Paper or 2 for Scissors.\n'))
 
-print(choice_list[user_choice])
-print('Computer chose:')
-print(computer_choice)
-if choice_list[user_choice] == choice_list[0]:
+if user_choice <= 2 and user_choice >= 0:
+	print(choice_list[user_choice])
+	print('Computer chose:')
+	print(computer_choice)
+
+if user_choice > 2 or user_choice < 0:
+	print("Invalid option. You Lose!")
+elif user_choice == choice_list.index(computer_choice):
+	print("It's a draw!")
+elif user_choice == 0:
 	if computer_choice == choice_list[1]:
 		print('You Lose!')
 	elif computer_choice == choice_list[2]:
 		print('You win!')
-elif choice_list[user_choice] == choice_list[1]:
+elif user_choice == 1:
 	if computer_choice == choice_list[0]:
 		print('You Win!')
 	elif computer_choice == choice_list[2]:
 		print('You Lose!')
-elif choice_list[user_choice] == choice_list[2]:
+elif user_choice == 2:
 	if computer_choice == choice_list[0]:
 		print('You Lose!')
 	elif computer_choice == choice_list[1]:
 		print('You Win!')
-else:
-	print("Invalid Option, You Lose!")
-
-if choice_list[user_choice] == computer_choice:
-	print("It's a draw!")
-	
