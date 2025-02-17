2b221dc89faf3e888c5ea888f16329f4b5988f6f
theLawrenzo1 <thelawrenzo1@gmail.com>
2025-02-17 01:38:43 +0000

    PYTHON FILE [ceaser_cipher1.py] - Modification

--- Day_08/ceaser_cipher1.py
+++ Day_08/ceaser_cipher1.py
@@ -2,9 +2,10 @@
 
 alphabet = [
 	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-	'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-	't', 'u', 'v', 'w', 'x', 'y', 'z'
+	'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+	's','t', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
+
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
@@ -12,18 +13,20 @@ shift = int(input("Type the shift number:\n"))
 
 
 def encrypt(original_text="Hello, World!", shift_amount=1):
-    encoded_text = ""
+	encoded_text = ""
 
-    if len(original_text) > 0:
-        for letter in original_text:
-            places = alphabet.index(letter) + shift_amount + 1
+	for letter in original_text:
+		places = shift_amount + alphabet.index(letter)
 
-            if shift_amount <= 13 >= alphabet.index(letter):
-                encoded_text += alphabet[places]
-            elif shif_amount > 13 < alphabet.index(letter):
-                encoded_text += alphabet[places - (len(alphabet) - 1)]
-            elif 
+		if places > len(alphabet):
+			places -= len(alphabet)
+			encoded_text += alphabet[places]
+		elif places < len(alphabet):
+			encoded_text += alphabet[places]
+		else:
+	 		encoded_text += alphabet[places - len(alphabet)]
 
-    print(f'Here is the encoded result: {encoded_text}')
+	print(f'Here is the encoded result: {encoded_text}')
+
 
 encrypt(text, shift)
